220aef2cd362ab22d3fb0b5550fe1cb972208437
Iakov Matiushin <imatiushin@openweathermap.org>
2023-09-01 17:07:52 +0300

    Added click, client configuration and custom kwargs passing

--- deker_shell/main.py
+++ deker_shell/main.py
@@ -19,8 +19,10 @@ import datetime  # noqa F401
 import runpy
 import sys
 
-from typing import TYPE_CHECKING, Optional  # noqa: I101
+from typing import TYPE_CHECKING, Optional, Union, Any  # noqa: I101
 
+import click as click
+import deker
 import numpy as np  # noqa F401
 
 from deker import *  # noqa F403
@@ -39,14 +41,21 @@ collection: Optional[Collection] = None  # default collection variable, set by u
 client: Optional[Client] = None  # default variable for Client instance
 
 
-async def interactive_shell(uri: str) -> None:
+async def interactive_shell(
+    uri: str,
+    **kwargs: Any
+) -> None:
     """Coroutine that starts a Python REPL from which we can access the Deker interface.
 
-    :param uri: client uri
+    :param uri: uri to Deker storage
+    :param kwargs: Client parameters
     """
     global client
     try:
-        client = Client(uri)
+        client = Client(
+            uri,
+            **kwargs
+        )
         collections: list[str] = [coll.name for coll in client]
 
         def use(name: str) -> None:
@@ -84,16 +93,57 @@ async def interactive_shell(uri: str) -> None:
                 pass
 
 
-def start() -> None:
+@click.command(context_settings=dict(ignore_unknown_options=True, allow_extra_args=True))
+@click.argument('uri', required=True, type=str)
+@click.option('--workers', type=int, help='Number of threads for Deker.')
+@click.option('--write-lock-timeout', type=int, help='An amount of seconds during which a parallel writing process waits for release of the locked file.')
+@click.option('--write-lock-check-interval', type=int, help='An amount of time (in seconds) during which a parallel writing process sleeps between checks for locks.')
+@click.option('--loglevel', type=str, help='Level of Deker loggers.')
+@click.option('--memory-limit', type=str, help='Limit of memory allocation per one array/subset in bytes or in human representation of kilobytes, megabytes or gigabytes, e.g. "100K", "512M", "4G". Human representations will be converted into bytes. If result is <= 0, total RAM + total swap is used.')
+@click.pass_context
+def start(
+    ctx,
+    uri: str,
+    workers: Optional[int] = None,
+    write_lock_timeout: Optional[int] = None,
+    write_lock_check_interval: Optional[int] = None,
+    loglevel: Optional[str] = None,
+    memory_limit: Optional[Union[int, str]] = None
+) -> None:
     """Application entrypoint."""
-    if len(sys.argv) < 2:
-        print("No params passed")
-    elif sys.argv[1].endswith(".py"):
-        runpy.run_path(path_name=sys.argv[1])
+    if uri.endswith(".py"):
+        runpy.run_path(path_name=uri)
     else:
-        uri: str = sys.argv[1]
         validate_uri(uri)
-        asyncio.run(interactive_shell(uri))
+
+        # deker client default parameters
+        kwargs = {
+            "workers": workers,
+            "write_lock_timeout": write_lock_timeout,
+            "write_lock_check_interval": write_lock_check_interval,
+            "loglevel": loglevel,
+            "memory_limit": memory_limit
+        }
+        default_keys = tuple(kwargs.keys())
+        for key in default_keys:
+            if not kwargs.get(key):
+                kwargs.pop(key, None)
+
+        # extra parameters
+        if ctx.args:
+            for i in range(0, len(ctx.args), 2):
+                value = ctx.args[i + 1]
+                if "." in ctx.args[i]:
+                    kwargs_key, inner_key = ctx.args[i].split(".")
+                    kwargs[kwargs_key[2:]] = {}
+                    kwargs[kwargs_key[2:]][inner_key] = value
+                else:
+                    kwargs[ctx.args[i][2:]] = value
+
+        asyncio.run(interactive_shell(
+            uri,
+            **kwargs
+        ))
 
 
 if __name__ == "__main__":
